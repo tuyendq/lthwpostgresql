d890ec3a1860be4b4ae364e234052e5ab33e84cc
Tuyen Dang <tuyendq@gmail.com>
2025-05-17 21:35:00 +0700

    x220: connect.py using psycopg version 3.x

--- src/connect.py
+++ src/connect.py
@@ -1,16 +1,35 @@
-import psycopg2
-from config import load_config
+import psycopg  # psycopg is the new version of psycopg2 (https://www.psycopg.org/)
+from dotenv import load_dotenv
+import os
 
-def connect(config):
+# from config import load_config
+
+def connect():
     """Connect to the PostgreSQL database server."""
+    load_dotenv()
     conn = None
     try:
-        with psycopg2.connect(**config) as conn:
-            print("Connection successful")
+        dbname=os.getenv("PGDATABASE")
+        user=os.getenv("PGUSER")
+        password=os.getenv("PGPASSWORD") 
+        host=os.getenv("PGHOST") 
+        port=os.getenv("PGPORT")
+        endpointid=os.getenv("ENDPOINTID")
+        with psycopg.connect(
+            dbname=os.getenv("PGDATABASE"), 
+            user=os.getenv("PGUSER"),
+            password=os.getenv("PGPASSWORD"),
+            host=os.getenv("PGHOST"),
+            port=os.getenv("PGPORT"),
+            options='endpoint=' + os.getenv("ENDPOINTID")
+            ) as conn:
+            print(f"Connection successfully {dbname} database on {host} as {user}.")
             return conn
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception, psycopg.DatabaseError) as error:
         print(error)
+        
 
 if __name__ == '__main__':
-    config = load_config()
-    connect(config)
+    conn = connect()
+    print(type(conn))
+    
